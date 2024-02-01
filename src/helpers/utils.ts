e9abbe755a48a7d691d7120c3c217ae330106510
beyerenrico <enrico.beyer@synaigy.com>
2024-02-01 14:46:48 +0100

    Add utilities to check if day has events and to retrieve them

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,3 +1,17 @@
+import { isSameDay } from "date-fns";
+
 export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
+}
+
+export function dayHasEvents(day: Date, events: CalendarEvent[]) {
+  return events.some((event) => {
+    return isSameDay(day, event.startDate);
+  })
+}
+
+export function eventsForDay(day: Date, events: CalendarEvent[]) {
+  return events.filter((event) => {
+    return isSameDay(day, event.startDate);
+  })
 }
