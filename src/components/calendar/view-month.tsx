7db4b925a1769787699fdeadaee7a8ac1d4f2c21
beyerenrico <enrico.beyer@synaigy.com>
2024-02-02 18:55:13 +0100

    Add hover effect

--- src/components/calendar/view-month.tsx
+++ src/components/calendar/view-month.tsx
@@ -65,7 +65,7 @@ const ViewMonth: React.FC<ViewMonthProps> = ({ onAddEvent, onViewChange }) => {
                 }}
                 className={classNames(
                   isSameMonth(day, firstDayCurrentMonth) ? 'bg-white' : 'bg-gray-50 text-gray-500',
-                  'relative px-3 py-2 flex items-start justify-start flex-col overflow-hidden'
+                  'relative px-3 py-2 flex items-start justify-start flex-col overflow-hidden hover:bg-gray-100'
                 )}
               >
                 <time
@@ -114,7 +114,7 @@ const ViewMonth: React.FC<ViewMonthProps> = ({ onAddEvent, onViewChange }) => {
                   !isEqual(day, selectedDate) && isEqual(day, today) ? 'text-indigo-600' : '',
                   !isEqual(day, selectedDate) && isSameMonth(day, firstDayCurrentMonth) && !isEqual(day, today) ? 'text-gray-900' : '',
                   !isEqual(day, selectedDate) && !isSameMonth(day, firstDayCurrentMonth) && !isEqual(day, today) ? 'text-gray-500' : '',
-                  'flex h-14 flex-col px-3 py-2 hover:bg-gray-100 focus:z-10'
+                  'flex h-14 flex-col px-3 py-2 hover:bg-gray-100 focus:z-10 hover:bg-gray-100'
                 )}
               >
                 <time
